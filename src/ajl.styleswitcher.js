7450bd3a91c36f2c2d81204f5c4ece66b18fa770
Hideki Abe <hideki.abe.0630@gmail.com>
2015-07-27 17:42:37 +0900

    StyleSwitcher開発完了

--- src/ajl.styleswitcher.js
+++ src/ajl.styleswitcher.js
@@ -25,67 +25,86 @@ ajl.StyleSwitcher = function (elem, options) {
 ajl.StyleSwitcher.prototype = {
     _altStyles: [],
 
-    temp2: function (title) {
-        if (title !== this.activeTitle) {
-            document.querySelector("link[title='" + this.activeTitle + "']").disabled = true;
-            document.querySelector("link[title='" + title + "']").disabled = false;
-            this.activeTitle = title;
+    _getAnchor: function (title) {
+        return document.querySelector("a[href='#" + title + "']");
+    },
 
-            // 初期ロード時は不要
-            ajl.cookie.setItem(this.options.key, title);
+    _setActiveClass: function (afterElem, beforeElem) {
+        ajl.util.addClass(afterElem, "active");
+
+        if (beforeElem) {
+            ajl.util.removeClass(beforeElem, "active");
         }
     },
 
-    temp: function (e) {
-        // console.log(e.target);
+    changeStyle: function (title) {
+        var d = document;
+
+        d.querySelector("link[title='" + this.activeTitle + "']").disabled = true;
+        d.querySelector("link[title='" + title + "']").disabled = false;
+        this.activeTitle = title;
+    },
+
+    changeStyleHandler: function (e) {
         var target = e.target;
         var title = target.getAttribute("href").replace("#", "");
 
         e.preventDefault();
 
         if (title !== this.activeTitle) {
-            document.querySelector("link[title='" + this.activeTitle + "']").disabled = true;
-            document.querySelector("link[title='" + title + "']").disabled = false;
-            this.activeTitle = title;
+            this._setActiveClass(target, this._getAnchor(this.activeTitle));
+            this.changeStyle(title);
             ajl.cookie.setItem(this.options.key, title);
         }
     },
 
     init: function () {
-        this.styles = this.options.collect.call(this, this.elem);
-        Array.prototype.forEach.call(this.styles, ajl.util.proxy(this, function (elem) {
-            this.styleTitles.push(elem.getAttribute("href").replace("#", ""));    // e.g) Blue
-        }));
+        var styles;
 
         if (!ajl.StyleSwitcher.prototype._altStyles.length) {
-            var styles = document.querySelectorAll("html > head > link[rel='alternate stylesheet']");
+            styles = document.querySelectorAll("link[rel='alternate stylesheet']");
             Array.prototype.forEach.call(styles, ajl.util.proxy(this, function (elem) {
-                var href = elem.getAttribute("href");
-                var title = elem.getAttribute("title");    // e.g) Blue
-                var link = document.createElement("link");
+                var href = elem.getAttribute("href"),
+                    title = elem.getAttribute("title"),
+                    link = document.createElement("link");
 
-                if (this.styleTitles.indexOf(title)) {
-                    link.setAttribute("rel", "stylesheet");
-                    link.setAttribute("href", href);
-                    link.setAttribute("title", title);
-                    link.disabled = true;
-                    elem.parentNode.replaceChild(link, elem);
-                    ajl.StyleSwitcher.prototype._altStyles.push(title);
-                }
+                link.setAttribute("rel", "stylesheet");
+                link.setAttribute("href", href);
+                link.setAttribute("title", title);
+                link.disabled = true;
+                elem.parentNode.replaceChild(link, elem);
+                ajl.StyleSwitcher.prototype._altStyles.push(title);
             }));
         }
 
-        this.styleTitles.forEach(ajl.util.proxy(this, function (title) {
+        this.styles = this.options.collect.call(this, this.elem);
+        Array.prototype.forEach.call(this.styles, ajl.util.proxy(this, function (elem) {
+            var title = elem.getAttribute("href").replace("#", "");
+
+            this.styleTitles.push(title);
+
             if (ajl.StyleSwitcher.prototype._altStyles.indexOf(title) === -1) {
                 this.activeTitle = title;
+                this._setActiveClass(elem);
             }
         }));
 
-        ajl.event.add(this.elem, "click", ajl.util.proxy(this, this.temp));
+        ajl.event.add(
+            this.elem,
+            "click",
+            ajl.util.proxy(this, this.changeStyleHandler)
+        );
 
         if (ajl.cookie.hasItem(this.options.key)) {
             var title = ajl.cookie.getItem(this.options.key);
-            this.temp2(title);
+
+            if (title !== this.activeTitle) {
+                this._setActiveClass(
+                    this._getAnchor(title),
+                    this._getAnchor(this.activeTitle)
+                );
+                this.changeStyle(title);
+            }
         }
     }
 };
